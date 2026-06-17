1573e36ccda217a6d649e7374c527242664b3e18
Nivetha <nive.r489@gmail.com>
2026-06-17 15:08:28 +0530

    event

---
+++ event.js
@@ -0,0 +1,29 @@
+console.log("A")
+setTimeout(()=>{
+    console.log("D")
+}, 2000)
+setTimeout(()=>{
+    console.log("B")
+}, 0)
+console.log("C")
+
+
+
+
+function example(){
+    console.log("hi")
+}
+
+const example1 = ()=>{
+    console.log("hello")
+}
+const promise = new Promise((resolve, reject) => {
+    setTimeout(example, 4500)
+    setTimeout(example1, 0)
+
+
+})
+
+promise.then(res=>{console.log(res)})
+.catch(err=>console.log(err))
+
